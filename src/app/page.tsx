c875b7478d3ea8ef70d996fb0c7ec72ee2b0ee81
rayhaanfarooq <rayhaanfarooq2004@gmail.com>
2024-07-31 15:51:42 -0400

    fix: removed incorrect project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <section id="projects">
+      {/* <section id="projects">
         <div className="space-y-12 w-full py-12">
           <BlurFade delay={BLUR_FADE_DELAY * 11}>
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -156,7 +156,7 @@ export default function Page() {
             ))}
           </div>
         </div>
-      </section>
+      </section> */}
 
       <section id="clubs">
 
